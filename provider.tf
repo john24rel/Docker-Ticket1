59c61bb3d19fc5dfd6128c86fe34b8ed8cbb5be4
Vladlen Rodionov <72477919+vrodi18@users.noreply.github.com>
2020-12-21 15:22:05 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "${var.region}"
-  shared_credentials_file = "/root/.aws/credentials"
+  #shared_credentials_file = "/root/.aws/credentials"
 }
 terraform {
   required_version = "0.11.13"
