f1c44817976a7eeffda8f96518f87a0f565e95b9
Vladlen Rodionov <72477919+vrodi18@users.noreply.github.com>
2020-12-22 12:34:32 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
 variable "instance_type" {}
-variable "key_name" {}   
+variable "key_name" {}
 variable "region" {}
+variable "subnet_id" {}
+variable "ami_id" {}
+variable "volume_size" {}
+variable "vpc_id" {}
+variable "cidr_blocks" {
+    type = "list"
+}
+variable "tags" {
+  type = "map"
+}
