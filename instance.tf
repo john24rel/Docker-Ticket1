011d82056999b95b0ff116141c3a824fb2f5342a
Vladlen Rodionov <72477919+vrodi18@users.noreply.github.com>
2020-12-22 12:35:49 -0600

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "Test" {
-  ami                         = "${data.aws_ami.centos.id}"
-  associate_public_ip_address = true
+  ami                         = "${var.ami_id}"
   instance_type               = "${var.instance_type}"
-  vpc_security_group_ids = ["${aws_security_group.ticket1.id}"]
+  vpc_security_group_ids      = ["${aws_security_group.ticket1.id}"]
   key_name                    = "${aws_key_pair.Ticket1.key_name}"
-  tags = {
-     Name = "Docker"
+  subnet_id                   = "${var.subnet_id}"
+  tags                        = "${var.tags}"
+  root_block_device {
+      volume_size = "${var.volume_size}"
   }
 }
