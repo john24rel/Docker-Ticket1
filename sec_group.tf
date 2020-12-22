7ed239ce70268fa6088898922aaf767c4f01a0b8
Vladlen Rodionov <72477919+vrodi18@users.noreply.github.com>
2020-12-22 12:35:22 -0600

    Update sec_group.tf

--- sec_group.tf
+++ sec_group.tf
@@ -1,20 +1,20 @@
-instance_type = "t2.micro"
-key_name      = "RFM"
-region        = "us-east-1"
-ami_id        = ""
-subnet_id     = ""
-volume_size   = "250"
-vpc_id        = ""
-cidr_blocks     = [
-    "10.1.0.0/16",
-    "10.2.0.0/16",
-    "100.100.0.0/16"
-]
-tags = {
-    Name        = "VPC_Project"
-    Environment = "Dev"
-    Team        = "DevOps"
-    Department  = "Operations"
-    Bill        = "CFO"
-    Quarter     = "1"
+resource "aws_security_group" "ticket1" {
+  name        = "RFM"
+  description = "Allow RFM"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.cidr_blocks}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
